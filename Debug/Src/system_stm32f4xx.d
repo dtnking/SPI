8df8b1545316da40a4de1bd4a45c5dcdf0d45000
Danny Aw <kokwah07@hotmail.com>
2017-11-21 21:25:57 +0800

    Created a SPI project and had some try out on Master-Slave communication

---
+++ Debug/Src/system_stm32f4xx.d
@@ -0,0 +1,79 @@
+Src/system_stm32f4xx.o: ../Src/system_stm32f4xx.c \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Inc/stm32f4xx_hal_conf.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Inc/main.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Inc/stm32f4xx_hal_conf.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Inc/main.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Student/akw/Project/c/Microcontroller\ Peripherals/SPI/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
